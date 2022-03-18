11cff845e4c3e6a16a2bd2b66ef6f33e15c7afd2
BlaCBunJEE <96791045+BlaCBunJEE@users.noreply.github.com>
2022-03-18 01:53:55 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+import time
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('snake game')
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_on = True
+
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # detect collision.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # detect collision
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_on = False
+        scoreboard.game_over()
+
+    # detect tail collision
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,33 @@
+from turtle import Turtle
+
+ALIGNMENT = 'center'
+FONT = ('courier', 15, 'normal')
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color('white')
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.score_update()
+
+    def score_update(self):
+        self.write(f'Score: {self.score}', align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.score_update()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write('GAME OVER', align=ALIGNMENT, font=FONT)
+
+
+
+
+
